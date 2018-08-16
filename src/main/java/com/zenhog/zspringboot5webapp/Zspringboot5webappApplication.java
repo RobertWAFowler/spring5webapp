5780e09d9ea2c80f12a555fdaaaea5bbd5eb9b3c
Robert Fowler <robertf@ipay.co.za>
2018-08-16 08:57:42 +0200

    Created branch and starter project for springboot5 udemy course.

---
+++ src/main/java/com/zenhog/zspringboot5webapp/Zspringboot5webappApplication.java
@@ -0,0 +1,12 @@
+package com.zenhog.zspringboot5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zspringboot5webappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zspringboot5webappApplication.class, args);
+    }
+}
--- src/test/java/com/zenhog/zspringboot5webapp/Zspringboot5webappApplicationTests.java
+++ src/test/java/com/zenhog/zspringboot5webapp/Zspringboot5webappApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp;
+package com.zenhog.zspringboot5webapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Spring5webappApplicationTests {
+public class Zspringboot5webappApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
