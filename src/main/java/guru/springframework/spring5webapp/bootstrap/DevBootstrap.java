a78dbe7872aced6c4dc39ce8cd7ce5960dd57a3a
Robert Fowler <robertf@ipay.co.za>
2018-08-18 13:45:41 +0200

    Add controllers for authors and books including thymeleaf views.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
     private PublisherRepository publisherRepository;
@@ -33,12 +32,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         Publisher publisher = new Publisher();
         publisher.setName("foo");
+        publisher.setAddress("12th Street, LA");
         publisherRepository.save(publisher);
 
         //Eric
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "1234", publisher);
-
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -48,7 +47,6 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         //Rod
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23444", publisher);
-
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
